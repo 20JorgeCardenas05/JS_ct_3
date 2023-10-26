1f4d2b2ee17168184a6afeaeea5ef42af2d1398f
20JorgeCardenas05 <jcardenas74@ucol.mx>
2023-10-25 18:45:14 -0600

    Second commit

--- app.js
+++ app.js
@@ -19,30 +19,70 @@
 //     document.write("Esa no es una edad");
 // }
 
-let dia = prompt("Dame el día de la semana").toLowerCase();
+// let dia = prompt("Dame el día de la semana").toLowerCase();
 
-switch (dia) {
-    case 'lunes':
-        console.log("EL día que seleccionaste es lunes");
-        break;
-    case 'martes':
-        console.log("EL día que seleccionaste es martes");
-        break;
-    case 'miércoles':
-        console.log("EL día que seleccionaste es miércoles");
-        break;
-    case 'jueves':
-        console.log("EL día que seleccionaste es jueves");
-        break;
-    case 'viernes':
-        console.log("EL día que seleccionaste es viernes");
-        break;
-    case 'sábado':
-        console.log("EL día que seleccionaste es sábado");
-        break;
-    case 'domingo':
-        console.log("EL día que seleccionaste es domingo");
-        break;
-    default:
-        console.log("Ese día no existe");
-}
+// switch (dia) {
+//     case 'lunes':
+//         console.log("EL día que seleccionaste es lunes");
+//         break;
+//     case 'martes':
+//         console.log("EL día que seleccionaste es martes");
+//         break;
+//     case 'miércoles':
+//         console.log("EL día que seleccionaste es miércoles");
+//         break;
+//     case 'jueves':
+//         console.log("EL día que seleccionaste es jueves");
+//         break;
+//     case 'viernes':
+//         console.log("EL día que seleccionaste es viernes");
+//         break;
+//     case 'sábado':
+//         console.log("EL día que seleccionaste es sábado");
+//         break;
+//     case 'domingo':
+//         console.log("EL día que seleccionaste es domingo");
+//         break;
+//     default:
+//         console.log("Ese día no existe");
+// }
+
+// OPERADOR TERNARIO
+// condición ? true : false
+
+// let num1 = 15;
+// let num2 = 20;
+
+// let valor = (num1 > num2) ? 'El número 1 es mayor.' : 'El número 2 es mayor.'
+
+// console.log(valor);
+
+// nume1 = prompt("Escribe un número");
+// nume2 = prompt("Escribe otro número");
+
+// if (nume1 == nume2) {
+//     console.log("Los números son iguales");
+// } else {
+//     if (nume1 > nume2) {
+//         console.log("El número " + nume1 + " es mayor que el número " + nume2);
+//     } else {
+//         console.log("El número " + nume2 + " es mayor que el número " + nume1);
+//     }
+// }
+
+// let n = prompt("Escribe el número que desees saber si es par o impar");
+
+// if ((n % 2) == 0) {
+//     console.log("El número es par.");
+// } else {
+//     console.log("El número es impar.");
+// }
+
+// nu1 = prompt("Escribe el número que quiera dividir.");
+// nu2 = prompt("Escribe el número con el que lo quieres dividir.");
+
+// if (nu1 == 0 || nu2 == 0) {
+//     console.log("Error");
+// } else {
+//     console.log(nu1 / nu2);
+// }
